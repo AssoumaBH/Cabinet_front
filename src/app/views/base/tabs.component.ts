2bf33a54bdc07923c3be92f2c1657e9cba38f063
AssoumaBH <Asma.242009@gmail.com>
2020-11-25 14:28:17 +0100

    Rendez_vous_patient

--- src/app/views/base/tabs.component.ts
+++ src/app/views/base/tabs.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 
 @Component({
   templateUrl: 'tabs.component.html'
 })
 export class TabsComponent {
-
+  @ViewChild('myModal') public myModal: ModalDirective;
   constructor() { }
 
 }
